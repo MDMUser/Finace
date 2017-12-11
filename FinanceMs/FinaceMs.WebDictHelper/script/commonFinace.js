60f6e83daf73d6c0f860188630354382cba93e5a
songxw <m17853523985@163.com>
2017-12-11 13:55:50 +0800

    针对级次调整功能局部刷新树形数据 增加了这一个方法

--- FinanceMs/FinaceMs.WebDictHelper/script/commonFinace.js
+++ FinanceMs/FinaceMs.WebDictHelper/script/commonFinace.js
@@ -21,5 +21,48 @@
         objPara.Owner = "";
         objPara.Description = "";
         return objPara;
+    },
+    /**
+    * 针对级次调整功能局部刷新树形数据
+    * 传了三个参数（$treeList,self,newParentObj）
+    */
+    partialRefreshForAdjust: function ($treeList, self, newParentObj) {
+        var treegrid = $treeList,
+            model = self.defaultModel(),
+            idKey = self.treeGridHelper.getIDField(model),
+            layerKey = model.layerKey,
+            node = $treeList.treegrid('getSelected'),
+            layerValue = parseInt(node[layerKey], 10),
+            oldParentObj = $treeList.treegrid('getParent', node.FJM);
+        // 局部刷新 old parent
+        if (node.Layer == 1) { // 当前点的级数的值等于1的时候
+            // 移除当前的数据 
+            self.treeGridHelper.removeNode(treegrid, model, node);
+            self.getListDataSourceWithOtherFilterCondition(newParentObj.FJM, newParentObj.Layer + 1).then(function (ds) {
+                if (ds) {
+                    self.treeGridHelper.appendNodeList(treegrid, newParentObj, model, ds.tables(0).peek(), false);
+                    self.treeGridHelper.dealExpand(treegrid, model, newParentObj.FJM);
+                }
+            })
+        }
+        else if (newParentObj == null) { //新父节点为空的时候 
+            self.Method_Load();
+        }
+        else {
+            // 局部刷新 old parent  
+            self.getListDataSourceWithOtherFilterCondition(oldParentObj.FJM, layerValue).then(function (ds) {
+                if (ds) {
+                    self.treeGridHelper.appendNodeList(treegrid, oldParentObj, model, ds.tables(0).peek(), false);
+                    self.treeGridHelper.dealExpand(treegrid, model, oldParentObj.FJM);
+                }
+            });
+            // 局部刷新 new parent  
+            self.getListDataSourceWithOtherFilterCondition(newParentObj.FJM, newParentObj.Layer + 1).then(function (ds) {
+                if (ds) {
+                    self.treeGridHelper.appendNodeList(treegrid, newParentObj, model, ds.tables(0).peek(), false);
+                    self.treeGridHelper.dealExpand(treegrid, model, newParentObj.FJM);
+                }
+            });
+        }
     }
 };
